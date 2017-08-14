26386f3289fb208b959420a316019946ccf7958f
James Waite <James Waite>
2017-08-14 14:25:51 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,5 @@ var Quiz = function(){
   var self = this;
 }
 
-function openNav() {
-    document.getElementById("mySidenav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.height = "0";
-}
-
 var quiz = new Quiz();
 quiz.init();
