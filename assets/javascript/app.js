bd769587631d90aef300baeb3504e9188c3ae228
James Waite <James Waite>
2017-08-14 17:04:07 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
-var Quiz = function(){
-  var self = this;
-}
+// var Quiz = function(){
+//   var self = this;
+// }
+// var quiz = new Quiz();
+// quiz.init();
 
-var quiz = new Quiz();
-quiz.init();
+var quiz = {
+  time: 180;
+  numCorrect: 0;
+  numIncorrect: 0;
+  numComplete: 0;
+  numIncomplete: 0;
+
+  startTimer function() {
+    $("#startTimer").on("click", ) {
+      for (i = this.time; i >= 0; i--;)
+        $("#timer").html("i");
+    };
+  }
+}
